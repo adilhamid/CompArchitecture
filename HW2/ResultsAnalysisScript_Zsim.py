a310c737a6934c5af4a29bafa94513f362b84bf2
adilhamid <aaddil.hamid@gmail.com>
2017-09-29 15:03:00 -0500

    New script added

--- HW2/ResultsAnalysisScript_Zsim.py
+++ HW2/ResultsAnalysisScript_Zsim.py
@@ -1,35 +1,70 @@
-import h5py
 import numpy as np
+import h5py
+import os
 from matplotlib import pyplot as plt
 
-# Open stats file
-fileA2 = h5py.File("H:/Courses/CSCE614/casim/zsim/outputs/hw2/A2/blackscholes_8c_simsmall/zsim.h5", "r")
-fileA3 = h5py.File("H:/Courses/CSCE614/casim/zsim/outputs/hw2/A3/blackscholes_8c_simsmall/zsim.h5", "r")
+A2file="H:/Courses/CSCE614/casim/zsim/outputs/hw2/A2/blackscholes_8c_simsmall/zsim.h5"
+A3file="H:/Courses/CSCE614/casim/zsim/outputs/hw2/A3/blackscholes_8c_simsmall/zsim.h5"
 
-# Get the single dataset in the file
+A2ben=os.listdir("/Users/punu/Desktop/sims/hw2/A2/")[1:]
+A2benpaths=[]
+for i in range(len(A2ben)):
+	A2benpaths.append("/Users/punu/Desktop/sims/hw2/A2/"+str(A2ben[i])+"/zsim.h5")
+	
+A3ben=os.listdir("/Users/punu/Desktop/sims/hw2/A3/")[1:]
+A3benpaths=[]
+for i in range(len(A3ben)):
+	A3benpaths.append("/Users/punu/Desktop/sims/hw2/A3/"+str(A3ben[i])+"/zsim.h5")
 
-A2stats = fileA2["stats"]
-A3stats = fileA3["stats"]
-A2root = A2stats["root"]
-A3root = A3stats["root"]
-A2core = A2root["westmere"]
-A3core = A3root["westmere"]
-A2mispred = A2core["mispredBranches"]
+prediction_accuracy_A2={}
+cycles_A2={}
+for i in range(len(A2benpaths)):
+	f=h5py.File(A2benpaths[i],"r")
+	number_of_mispred_branches=np.sum(f["stats"]["root"]["westmere"]["mispredBranches"][-1])
+	number_of_cond_branches=np.sum(f["stats"]["root"]["westmere"]["condBranches"][-1])
+	number_of_cCycles=np.array(f["stats"]["root"]["westmere"]["cCycles"][-1])
+	number_of_Cycles=np.array(f["stats"]["root"]["westmere"]["cycles"][-1])
+	cycles_A2[A2ben[i]]=np.max(number_of_cCycles+number_of_Cycles)
+	prediction_accuracy_A2[A2ben[i]]=100*(1-(float(number_of_mispred_branches)/number_of_cond_branches))
 
-print(A2mispred)
-A3mispred = np.array(A3core["mispredBranches"],dtype=float)
-#A2condBran=A2core["condBranches"]
-A3condBran=np.array(A3core["condBranches"],dtype=float)
-#A2misrate=float(A2mispred)/A2condBran
-A3misrate=(A3mispred[3])/A3condBran[3]
 
-A3predacc=1-A3misrate
-print(A3predacc)
-plt.plot(A3predacc,'rx-')
+
+#Same functions but now for A3
+
+prediction_accuracy_A3={}
+cycles_A3={}
+for i in range(len(A3benpaths)):
+	f=h5py.File(A3benpaths[i],"r")
+	number_of_mispred_branches=np.sum(f["stats"]["root"]["westmere"]["mispredBranches"][-1])
+	number_of_cond_branches=np.sum(f["stats"]["root"]["westmere"]["condBranches"][-1])
+	number_of_cCycles=np.array(f["stats"]["root"]["westmere"]["cCycles"][-1])
+	number_of_Cycles=np.array(f["stats"]["root"]["westmere"]["cycles"][-1])
+	cycles_A3[A3ben[i]]=np.max(number_of_cCycles+number_of_Cycles)
+	prediction_accuracy_A3[A3ben[i]]=100*(1-(float(number_of_mispred_branches)/number_of_cond_branches))
+
+fig,ax=plt.subplots()
+plt.bar(np.arange(len(prediction_accuracy_A2)),prediction_accuracy_A2.values(),width=0.5,color="green",label="A2")
+plt.hold(True)
+plt.bar(0.5+np.arange(len(prediction_accuracy_A3)),prediction_accuracy_A3.values(),width=0.5,color="red",label="A3")
+plt.xticks(range(len(prediction_accuracy_A2)),prediction_accuracy_A2.keys())
+plt.legend()
+plt.xlabel("Benchmark")
+plt.ylabel("Accuracy (%)")
+plt.grid(True)
+plt.title("Comparison of Branch prediction Accuracy")
+for ticks in ax.get_xticklabels():
+	ticks.set_rotation(-90)
+plt.figure(1)
+fig,ax=plt.subplots()
+for i in A2ben:
+	cycles_A3[i]=float(cycles_A3[i])/cycles_A2[i]
+plt.bar(np.arange(len(cycles_A3)),cycles_A3.values(),color="red")
+plt.xticks(np.arange(len(cycles_A3)),cycles_A3.keys())
+plt.xlabel("Benchmark")
+plt.ylabel("Normalized time taken by A3 compared to A2")
+plt.title("Comparison of Time taken by A3 relative to A2")
+plt.grid(True)
+for tick in ax.get_xticklabels():
+	tick.set_rotation(-90)
+
 plt.show()
-#
-# plt.bar(range(len(A2mispred[3])),A2mispred[3],color="blue")
-# plt.hold(True)
-# plt.figure(2)
-# plt.bar(range(len(A3mispred[3])),A3mispred[3],color="red",width=0.5)
-# plt.show()
