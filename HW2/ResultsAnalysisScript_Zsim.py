d29a949c40517698cc4401ff6b7ffcfd425444ff
adilhamid <aaddil.hamid@gmail.com>
2017-09-29 19:19:39 -0500

    Adding the HW2 report

--- HW2/ResultsAnalysisScript_Zsim.py
+++ HW2/ResultsAnalysisScript_Zsim.py
@@ -3,18 +3,19 @@ import h5py
 import os
 from matplotlib import pyplot as plt
 
-A2file="H:/Courses/CSCE614/casim/zsim/outputs/hw2/A2/blackscholes_8c_simsmall/zsim.h5"
-A3file="H:/Courses/CSCE614/casim/zsim/outputs/hw2/A3/blackscholes_8c_simsmall/zsim.h5"
 
-A2ben=os.listdir("/Users/punu/Desktop/sims/hw2/A2/")[1:]
+A2ben=os.listdir("Z:/Courses/CSCE614/casim/zsim/outputs/hw2/A2/")[0:]
+
 A2benpaths=[]
 for i in range(len(A2ben)):
-	A2benpaths.append("/Users/punu/Desktop/sims/hw2/A2/"+str(A2ben[i])+"/zsim.h5")
+	A2benpaths.append("Z:/Courses/CSCE614/casim/zsim/outputs/hw2/A2/"+str(A2ben[i])+"/zsim.h5")
 	
-A3ben=os.listdir("/Users/punu/Desktop/sims/hw2/A3/")[1:]
+A3ben=os.listdir("Z:/Courses/CSCE614/casim/zsim/outputs/hw2/A3/")[0:]
+for val in A3ben:
+	print val
 A3benpaths=[]
 for i in range(len(A3ben)):
-	A3benpaths.append("/Users/punu/Desktop/sims/hw2/A3/"+str(A3ben[i])+"/zsim.h5")
+	A3benpaths.append("Z:/Courses/CSCE614/casim/zsim/outputs/hw2/A3/"+str(A3ben[i])+"/zsim.h5")
 
 prediction_accuracy_A2={}
 cycles_A2={}
@@ -43,11 +44,14 @@ for i in range(len(A3benpaths)):
 	prediction_accuracy_A3[A3ben[i]]=100*(1-(float(number_of_mispred_branches)/number_of_cond_branches))
 
 fig,ax=plt.subplots()
-plt.bar(np.arange(len(prediction_accuracy_A2)),prediction_accuracy_A2.values(),width=0.5,color="green",label="A2")
+width = 0.35
+rect1 = plt.bar(np.arange(len(prediction_accuracy_A2)),prediction_accuracy_A2.values(), width,color="green")
 plt.hold(True)
-plt.bar(0.5+np.arange(len(prediction_accuracy_A3)),prediction_accuracy_A3.values(),width=0.5,color="red",label="A3")
-plt.xticks(range(len(prediction_accuracy_A2)),prediction_accuracy_A2.keys())
-plt.legend()
+rect2 = plt.bar(width+np.arange(len(prediction_accuracy_A3)),prediction_accuracy_A3.values(),width,color="red")
+
+plt.xticks(range(len(prediction_accuracy_A2)),prediction_accuracy_A2.keys()) 
+plt.legend((rect1[0], rect2[0]),('A2','A3'))
+
 plt.xlabel("Benchmark")
 plt.ylabel("Accuracy (%)")
 plt.grid(True)
@@ -55,10 +59,23 @@ plt.title("Comparison of Branch prediction Accuracy")
 for ticks in ax.get_xticklabels():
 	ticks.set_rotation(-90)
 plt.figure(1)
+
+def autolabel(rects):
+    for rect in rects:
+        height = rect.get_height()
+        plt.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+                '%f' % float(height),
+                ha='center', va='bottom', rotation = 90)
+autolabel(rect1)
+autolabel(rect2)
+
+plt.savefig('figure1.pdf', dpi=1000)
+
 fig,ax=plt.subplots()
 for i in A2ben:
 	cycles_A3[i]=float(cycles_A3[i])/cycles_A2[i]
-plt.bar(np.arange(len(cycles_A3)),cycles_A3.values(),color="red")
+
+rect1 = plt.bar(np.arange(len(cycles_A3)),cycles_A3.values(),color="blue")
 plt.xticks(np.arange(len(cycles_A3)),cycles_A3.keys())
 plt.xlabel("Benchmark")
 plt.ylabel("Normalized time taken by A3 compared to A2")
@@ -66,5 +83,9 @@ plt.title("Comparison of Time taken by A3 relative to A2")
 plt.grid(True)
 for tick in ax.get_xticklabels():
 	tick.set_rotation(-90)
+
+autolabel(rect1)
+
+plt.savefig('figure2.pdf', dpi=1000)
 
 plt.show()
